2617afb3ab33ce77919d7709a7f0a4a14d4e5296
Christian Cavasin <me@anluin.de>
2023-01-29 18:55:28 +0100

    Lamps are now placeable on all block faces and electric heater transforms have been adjusted

--- src/Content/Block/Entity/Behavior/Lamp.cs
+++ src/Content/Block/Entity/Behavior/Lamp.cs
@@ -5,9 +5,9 @@ using Vintagestory.API.Common;
 
 namespace Electricity.Content.Block.Entity.Behavior {
     public sealed class Lamp : BlockEntityBehavior, IElectricConsumer {
-        private int lightLevel;
-
         public Lamp(BlockEntity blockEntity) : base(blockEntity) { }
+
+        public int LightLevel { get; private set; }
 
         public ConsumptionRange ConsumptionRange {
             get => new ConsumptionRange(1, 8);
@@ -15,14 +15,14 @@ namespace Electricity.Content.Block.Entity.Behavior {
 
         public void Consume(int lightLevel) {
             if (this.Api is { } api)
-                if (lightLevel != this.lightLevel) {
-                    if (this.lightLevel == 0 && lightLevel > 0) {
+                if (lightLevel != this.LightLevel) {
+                    if (this.LightLevel == 0 && lightLevel > 0) {
                         var assetLocation = this.Blockentity.Block.CodeWithVariant("state", "enabled");
                         var block = api.World.BlockAccessor.GetBlock(assetLocation);
                         api.World.BlockAccessor.ExchangeBlock(block.Id, this.Blockentity.Pos);
                     }
 
-                    if (this.lightLevel > 0 && lightLevel == 0) {
+                    if (this.LightLevel > 0 && lightLevel == 0) {
                         var assetLocation = this.Blockentity.Block.CodeWithVariant("state", "disabled");
                         var block = api.World.BlockAccessor.GetBlock(assetLocation);
                         api.World.BlockAccessor.ExchangeBlock(block.Id, this.Blockentity.Pos);
@@ -35,15 +35,15 @@ namespace Electricity.Content.Block.Entity.Behavior {
                     };
 
                     this.Blockentity.MarkDirty(true);
-                    this.lightLevel = lightLevel;
+                    this.LightLevel = lightLevel;
                 }
         }
 
         public override void GetBlockInfo(IPlayer forPlayer, StringBuilder stringBuilder) {
             base.GetBlockInfo(forPlayer, stringBuilder);
 
-            stringBuilder.AppendLine(StringHelper.Progressbar(this.lightLevel * 100.0f / 8.0f));
-            stringBuilder.AppendLine("└ Consumption: " + this.lightLevel + "/" + 8 + "⚡   ");
+            stringBuilder.AppendLine(StringHelper.Progressbar(this.LightLevel * 100.0f / 8.0f));
+            stringBuilder.AppendLine("└ Consumption: " + this.LightLevel + "/" + 8 + "⚡   ");
             stringBuilder.AppendLine();
         }
     }
--- src/Content/Block/Entity/Lamp.cs
+++ src/Content/Block/Entity/Lamp.cs
@@ -1,16 +1,50 @@
+using System;
 using Electricity.Utils;
 using Vintagestory.API.Common;
+using Vintagestory.API.Datastructures;
+using Vintagestory.API.Util;
 
 namespace Electricity.Content.Block.Entity {
     public class Lamp : BlockEntity {
+        private Facing facing = Facing.None;
+
         private Behavior.Electricity Electricity {
             get => GetBehavior<Behavior.Electricity>();
         }
 
-        public override void OnBlockPlaced(ItemStack? byItemStack = null) {
-            base.OnBlockPlaced(byItemStack);
+        private Behavior.Lamp Behavior {
+            get => GetBehavior<Behavior.Lamp>();
+        }
 
-            this.Electricity.Connection = Facing.UpAll;
+        public Facing Facing {
+            get => this.facing;
+            set {
+                if (value != this.facing) {
+                    this.Electricity.Connection =
+                        FacingHelper.FullFace(this.facing = value);
+                }
+            }
+        }
+
+        public bool IsEnabled {
+            get => this.Behavior.LightLevel > 0;
+        }
+
+        public override void ToTreeAttributes(ITreeAttribute tree) {
+            base.ToTreeAttributes(tree);
+
+            tree.SetBytes("electricity:facing", SerializerUtil.Serialize(this.facing));
+        }
+
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldAccessForResolve) {
+            base.FromTreeAttributes(tree, worldAccessForResolve);
+
+            try {
+                this.facing = SerializerUtil.Deserialize<Facing>(tree.GetBytes("electricity:facing"));
+            } catch (Exception exception) {
+                this.facing = Facing.UpNorth;
+                this.Api?.Logger.Error(exception.ToString());
+            }
         }
     }
 }
