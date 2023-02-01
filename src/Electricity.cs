fcc37895b610026f82a0069a575b65d77e56f001
Christian Cavasin <me@anluin.de>
2023-02-01 22:31:47 +0100

    Bump version number to 0.0.6

--- src/Electricity.cs
+++ src/Electricity.cs
@@ -13,7 +13,7 @@ using Vintagestory.API.MathTools;
     "electricity",
     Website = "https://github.com/anluin/electricity",
     Description = "Brings electricity into the game!",
-    Version = "0.0.5",
+    Version = "0.0.6",
     Authors = new[] { "Anluin" }
 )]
 
